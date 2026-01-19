fa7fcae9ae01e951e14ceda85613c916d69f1a3d
Liam Sjögren <liamsjogren09@gmail.com>
2026-01-19 15:02:22 +0100

    Added menu

--- SecretApp/Program.cs
+++ SecretApp/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+
+        static string[] userNames = { "Pelle", "Stina", "Ali" };
+        static string[] userPasswords = { "1234", "abcd", "qwerty" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            AddUser();
+            ShowUsers();
+        }
+        static void AddUser()
+        {
+            Console.WriteLine("Hello from AddUser()");
+        }
+        static void ShowUsers()
+        {
+            int i = 0;
+            while (i < userNames.Length)
+            {
+                Console.WriteLine(userNames[i].ToUpper());
+                i++;
+            }
         }
     }
 }
